bc9d267fffdc79b90b193795e786911ee3bffce3
ahi6 <60984726+ahi6@users.noreply.github.com>
2025-03-29 18:59:40 +0100

    Change crate name

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Ok(Box::new(informatika_pokrocila_kalkulacka::CalculatorApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(ahi6_calculator::CalculatorApp::new(cc)))),
     )
 }
 
@@ -50,7 +50,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(informatika_pokrocila_kalkulacka::CalculatorApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(ahi6_calculator::CalculatorApp::new(cc)))),
             )
             .await;
 
